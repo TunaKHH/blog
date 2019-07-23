ee046785ec5e1bf63bad71526259bbf93b7de166
Tuna <df95111111@gmail.com>
2019-07-23 22:38:53 +0800

    將nav分割出來

---
+++ resources/views/todo/nav.blade.php
@@ -0,0 +1,33 @@
+<nav>
+    <div style="padding: 0em">
+        <img src="{{ asset('images/top.gif') }}" alt="" width="100%" >
+    </div>
+    <div class="row">
+        <div class="col nav-first">
+            <a href="{{ url('todo') }}">
+                <img src="{{ asset('images/nav/m-1a.jpg') }}" alt="" width="100%">
+            </a>
+        </div>
+        <div class="col nav">
+            <img src="{{ asset('images/nav/m-2a.jpg') }}" alt="" width="100%">
+        </div>
+        <div class="col nav">
+            <img src="{{ asset('images/nav/m-3a.jpg') }}" alt="" width="100%">
+        </div>
+        <div class="col nav">
+            <img src="{{ asset('images/nav/m-4a.jpg') }}" alt="" width="100%">
+        </div>
+        <div class="col nav">
+            <img src="{{ asset('images/nav/m-5a.jpg') }}" alt="" width="100%">
+        </div>
+        <div class="col nav">
+            <img src="{{ asset('images/nav/m-6a.jpg') }}" alt="" width="100%">
+        </div>
+        <div class="col nav">
+            <img src="{{ asset('images/nav/m-7a.jpg') }}" alt="" width="100%">
+        </div>
+        <div class="col nav-end">
+            <img src="{{ asset('images/nav/m-8a.jpg') }}" alt="" width="100%">
+        </div>
+    </div>
+</nav>
